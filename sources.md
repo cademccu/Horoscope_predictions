9a55987beaa39289af5307f8a189cda009874fb1
cademccu <cade21mccumber@gmail.com>
2021-09-12 14:43:53 -0600

    add initial sources and constants

---
+++ sources.md
@@ -0,0 +1,9 @@
+### SOURCES
+
+https://mystarshoroscopes.com/category/aquarius/aquarius-daily-horoscope/
+
+This endpoint works to fetch archives of pages of horoscopes via the format:
+
+```
+https://mystarshoroscopes.com/category/<NAME_LOWERCASE_SIGN>/<NAME_LOWERCASE_SIGN>-horoscope/pages/<2......N>
+```
---
+++ src/constants.py
@@ -0,0 +1,20 @@
+
+SIGNS = [
+        "aquarius",
+        "pisces",
+        "aries",
+        "taurus",
+        "gemini",
+        "cancer",
+        "leo",
+        "virgo",
+        "libra",
+        "scorpio",
+        "sagittarius",
+        "capricorn"
+        ]
+
+
+URL = "https://mystarshoroscopes.com/category/${SIGN}/${SIGN}-daily-horoscope/page/${PAGE_NUMBER}/"
+SIGN = "${SIGN}"
+PAGE_NUMBER = "${PAGE_NUMBER}"
